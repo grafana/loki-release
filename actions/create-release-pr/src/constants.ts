0ad335cf7b13c6cb374d85ec05d127300c01edba
Trevor Whitney <trevorjwhitney@gmail.com>
2024-01-19 10:29:09 -0700

    feat: remove unused code

--- actions/create-release-pr/src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const DEFAULT_LABELS = ['autorelease: pending']
-export const DEFAULT_RELEASE_LABELS = ['autorelease: tagged']
-export const RELEASE_CONFIG_PATH = 'release.json'
-export const DEFAULT_CHANGELOG_PATH = 'CHANGELOG.md'
