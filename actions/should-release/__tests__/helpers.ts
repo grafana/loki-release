f0290a4b3be31f22725262485fa0201db6ccf738
Trevor Whitney <trevorjwhitney@gmail.com>
2024-01-19 10:43:40 -0700

    feat: a bit more cleanup

--- actions/should-release/__tests__/helpers.ts
+++ actions/should-release/__tests__/helpers.ts
@@ -1,8 +1,7 @@
-import { Commit } from 'release-please/build/src/commit'
-import { GitHub, GitHubTag } from 'release-please/build/src/github'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { GitHub } from 'release-please/build/src/github'
 import { Logger } from 'release-please/build/src/util/logger'
 import { LogFn } from '../src/util'
-import { SinonSandbox, SinonStub } from 'sinon'
 
 export async function mockGitHub(): Promise<GitHub> {
   return GitHub.create({
@@ -12,33 +11,6 @@ export async function mockGitHub(): Promise<GitHub> {
     token: 'fake-token',
     logger: new NoOpLogger()
   })
-}
-
-export function mockCommits(
-  sandbox: SinonSandbox,
-  github: GitHub,
-  commits: Commit[]
-): SinonStub {
-  async function* fakeGenerator() {
-    for (const commit of commits) {
-      yield commit
-    }
-  }
-
-  return sandbox.stub(github, 'mergeCommitIterator').returns(fakeGenerator())
-}
-
-export function mockTags(
-  sandbox: SinonSandbox,
-  github: GitHub,
-  tags: GitHubTag[]
-): SinonStub {
-  async function* fakeGenerator() {
-    for (const tag of tags) {
-      yield tag
-    }
-  }
-  return sandbox.stub(github, 'tagIterator').returns(fakeGenerator())
 }
 
 export class NoOpLogger implements Logger {
