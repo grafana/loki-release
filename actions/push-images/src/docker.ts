89a23b36492f0350f11652962d59e0866e94ff11
Paul Rogers <129207811+paul1r@users.noreply.github.com>
2025-01-29 13:13:36 -0500

    feat: more tag tweaks (#200)

--- actions/push-images/src/docker.ts
+++ actions/push-images/src/docker.ts
@@ -58,14 +58,14 @@ export function buildDockerPluginCommands(
       // Add latest tag for each platform if this is the latest version
       if (isLatest) {
         commands.push(
-          `docker plugin create ${repo}/${image}:latest-${shortPlatform} "${buildDir}"`,
+          //`docker plugin create ${repo}/${image}:latest-${shortPlatform} "${buildDir}"`,
           `docker plugin push "${repo}/${image}:latest-${shortPlatform}"`,
-          // Add major version tag (e.g., v2-amd64)
-          `docker plugin create ${repo}/${image}:v${version.major}-${shortPlatform} "${buildDir}"`,
-          `docker plugin push "${repo}/${image}:v${version.major}-${shortPlatform}"`,
-          // Add major.minor version tag (e.g., v2.9-amd64)
-          `docker plugin create ${repo}/${image}:v${version.major}.${version.minor}-${shortPlatform} "${buildDir}"`,
-          `docker plugin push "${repo}/${image}:v${version.major}.${version.minor}-${shortPlatform}"`
+          // Add major version tag (e.g., 2-amd64)
+          //`docker plugin create ${repo}/${image}:${version.major}-${shortPlatform} "${buildDir}"`,
+          `docker plugin push "${repo}/${image}:${version.major}-${shortPlatform}"`,
+          // Add major.minor version tag (e.g., 2.9-amd64)
+          //`docker plugin create ${repo}/${image}:${version.major}.${version.minor}-${shortPlatform} "${buildDir}"`,
+          `docker plugin push "${repo}/${image}:${version.major}.${version.minor}-${shortPlatform}"`
         )
       }
     }
@@ -122,12 +122,12 @@ export function buildCommands(
         commands.push(
           `docker tag ${repo}/${image}:${version.toString()}-${shortPlatform} ${repo}/${image}:latest-${shortPlatform}`,
           `docker push ${repo}/${image}:latest-${shortPlatform}`,
-          // Add major version tag (e.g., v2-amd64)
-          `docker tag ${repo}/${image}:${version.toString()}-${shortPlatform} ${repo}/${image}:v${version.major}-${shortPlatform}`,
-          `docker push ${repo}/${image}:v${version.major}-${shortPlatform}`,
-          // Add major.minor version tag (e.g., v2.9-amd64)
-          `docker tag ${repo}/${image}:${version.toString()}-${shortPlatform} ${repo}/${image}:v${version.major}.${version.minor}-${shortPlatform}`,
-          `docker push ${repo}/${image}:v${version.major}.${version.minor}-${shortPlatform}`
+          // Add major version tag (e.g., 2-amd64)
+          `docker tag ${repo}/${image}:${version.toString()}-${shortPlatform} ${repo}/${image}:${version.major}-${shortPlatform}`,
+          `docker push ${repo}/${image}:${version.major}-${shortPlatform}`,
+          // Add major.minor version tag (e.g., 2.9-amd64)
+          `docker tag ${repo}/${image}:${version.toString()}-${shortPlatform} ${repo}/${image}:${version.major}.${version.minor}-${shortPlatform}`,
+          `docker push ${repo}/${image}:${version.major}.${version.minor}-${shortPlatform}`
         )
       }
     }
@@ -144,21 +144,21 @@ export function buildCommands(
         m.replace(`:${version.toString()}-`, ':latest-')
       )
       const majorManifests = manifests.map(m =>
-        m.replace(`:${version.toString()}-`, `:v${version.major}-`)
+        m.replace(`:${version.toString()}-`, `:${version.major}-`)
       )
       const majorMinorManifests = manifests.map(m =>
         m.replace(
           `:${version.toString()}-`,
-          `:v${version.major}.${version.minor}-`
+          `:${version.major}.${version.minor}-`
         )
       )
       commands.push(
         `docker manifest create ${repo}/${image}:latest ${latestManifests.join(' ')}`,
         `docker manifest push ${repo}/${image}:latest`,
-        `docker manifest create ${repo}/${image}:v${version.major} ${majorManifests.join(' ')}`,
-        `docker manifest push ${repo}/${image}:v${version.major}`,
-        `docker manifest create ${repo}/${image}:v${version.major}.${version.minor} ${majorMinorManifests.join(' ')}`,
-        `docker manifest push ${repo}/${image}:v${version.major}.${version.minor}`
+        `docker manifest create ${repo}/${image}:${version.major} ${majorManifests.join(' ')}`,
+        `docker manifest push ${repo}/${image}:${version.major}`,
+        `docker manifest create ${repo}/${image}:${version.major}.${version.minor} ${majorMinorManifests.join(' ')}`,
+        `docker manifest push ${repo}/${image}:${version.major}.${version.minor}`
       )
     }
   }
--- cmd/loki/main.go
+++ cmd/loki/main.go
@@ -10,7 +10,7 @@ func main() {
 
 	uid, err := uuid.GenerateUUID()
 	if err != nil {
-		fmt.Println("Error generating a UUID")
+		fmt.Println("Error generating UUID")
 	}
 	fmt.Println("This is not really Loki, just meant to look like it. Here's a UUID: ", uid)
 }
