6e5bcdfa812f9cd48bde419051afe67f4bff8309
Christian Haudum <christian.haudum@gmail.com>
2026-05-22 11:57:18 +0200

    fixup! ci: Replace npm with yarn

--- actions/get-version.ts
+++ actions/get-version.ts
@@ -1,9 +1,24 @@
 import { readFileSync } from 'fs'
-const release = JSON.parse(readFileSync('./release.json', 'utf8'))
+
+let data: string
+try {
+  data = readFileSync('./release.json', 'utf8')
+} catch (err: any) {
+  console.error(`Failed to read file: ${err.message}`)
+  process.exit(1)
+}
+
+let release: any
+try {
+  release = JSON.parse(data)
+} catch (err: any) {
+  console.error(`Failed to parse JSON: ${err.message}`)
+  process.exit(1)
+}
 
 const version = release[0]?.title?.version
-
 if (!version) {
+  console.error(`release.json does not contain version`)
   process.exit(1)
 }
 
